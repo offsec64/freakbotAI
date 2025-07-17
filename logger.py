548b0c349ab8639062477ce4f94311495e4976bd
itsnotfridayet <jacedill@outlook.com>
2025-07-16 23:06:43 -0600

    Move IP logger to seperate script

---
+++ logger.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+DISCORD_CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
+OUTSIDE_PORT = os.getenv("OUTSIDE_PORT")
+ABSTRACT_API_KEY = os.getenv("ABSTRACT_API_KEY")
+
+app = Flask(__name__)
+
+# Replace with your actual Discord bot token and channel ID
+DISCORD_BOT_TOKEN = API_KEY
+DISCORD_CHANNEL_ID = DISCORD_CHANNEL_ID
+
+def send_ip_to_discord(ip):
+    url = f"https://discord.com/api/v10/channels/{DISCORD_CHANNEL_ID}/messages"
+    headers = {
+        "Authorization": f"Bot {DISCORD_BOT_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    json_data = {
+        "content": f"New visitor IP: `{ip}`"
+    }
+    response = requests.post(url, headers=headers, json=json_data)
+    
+    if response.status_code != 200 and response.status_code != 204:
+        print("Failed to send message to Discord:", response.text)
+
+# ---------- Flask Routes -----------
+
+@app.route("/", methods=["GET"])
+def render_page():
+    return render_template("index.html")
+
+@app.route("/reveal", methods=["POST"])
+def reveal_ip():
+
+    forwarded = request.headers.get('X-Forwarded-For', request.remote_addr)
+    ip = forwarded.split(',')[0].strip()
+
+    # Make a request to the Abstract API to get IP intelligence data and parse the response
+    response = requests.get(f"https://ip-intelligence.abstractapi.com/v1/?api_key={ABSTRACT_API_KEY}&ip_address=" + ip)
+    data = json.loads(response.text)
+
+    send_ip_to_discord(ip, data)
+
+    #return response.json()  # Return the JSON response directly from the Abstract API
+    return jsonify({"ip": ip})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=OUTSIDE_PORT)
